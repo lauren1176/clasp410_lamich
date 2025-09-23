fcc70ef83d80846dcaf164780ea872cbe5ab6fda
Lauren Richards <lamich@umich.edu>
2025-09-23 11:14:46 -0400

    Added central difference

--- lectures/lecture3_examples.py
+++ lectures/lecture3_examples.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 
 plt.style.use('seaborn-v0_8-colorblind')
 plt.ion()
+plt.close('all')
 
 dx = 0.1
 x = np.arange(0, 6 * np.pi, dx)
@@ -21,6 +22,19 @@ cosx = np.cos(x)
 fwd_diff = (sinx[1:] - sinx[:-1]) / dx # s1 - s0 ... sn - sn-1
 bkd_diff = (sinx[1:] - sinx[:-1]) / dx # s0 - sn ... sn - sn-1
 
+'''
+Central difference approx.
+index 1:  dy[1]/dt = (y[2] - y[0]) / (2 delta x)
+index -2: dy[-2]/dt = (y[-1] - y[-3]) / (2 delta x)
+'''
+
+cnt_diff = (sinx[2:] - sinx[:-2]) / (2*dx) 
+
 plt.plot(x, cosx, label=r'Analytical Derivative of $\sin{x}$')
 plt.plot(x[:-1], fwd_diff, label='Forward Difference Approx.')
 plt.plot(x[1:], bkd_diff, label='Backward Difference Approx.')
+plt.plot(x[1:-1], cnt_diff, label='Central Difference Approx.')
+plt.legend()
+
+# error = || analytical - numerical ||
+dxs = [2**-n for n in range(0, 10)]
