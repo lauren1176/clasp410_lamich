a0affa5eed816129493a7a8cc85d87ba0d2d7529
Lauren Richards <lamich@umich.edu>
2025-10-09 11:53:15 -0400

    Created lab3 file

---
+++ labs/Lab3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/ipython3
+
+'''
+Tools and methods for completing Lab 3 which is the best lab.
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.style.use('fivethirtyeight')
+plt.ion()
+plt.close('all')
+
